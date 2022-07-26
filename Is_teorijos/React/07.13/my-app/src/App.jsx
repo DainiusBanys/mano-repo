cf5cf53f188b1ae77786b2f33d0904de1d4b385f
Dainius Banys <b.dainius@gmail.com>
2022-07-26 22:01:26 +0300

    inputs

--- Is_teorijos/React/07.13/my-app/src/App.jsx
+++ Is_teorijos/React/07.13/my-app/src/App.jsx
@@ -1,34 +1,20 @@
+import { useEffect, useState } from 'react';
 import './App.scss';
-import { useState } from "react"
-import Buttons from './Components/008/Buttons';
-import View from './Components/008/View';
-import Hello from './Components/008/HelloWorld';
-import Hello2 from './Components/008/HelloWorld2';
-import SquareRound from './Components/006/SquareRound';
+import Text from './Components/010/Text';
+import Select from './Components/010/Select';
 
 function App() {
     
 
 
-    const [visible, setVisible] = useState('none')
-
-
-
-
     return (
         <div className="App">
             <header className="App-header">
-                <h1>State Uplifting Tesinys</h1>
-                <div className='Container'>
-                <View visible={visible} active={1} element={null}></View>
-                <View visible={visible} active={2} element={<Hello/>}></View>
-                <View visible={visible} active={3} element={<Hello2/>}></View>
-                </div>  
-                <Buttons setVisible={setVisible} ></Buttons>   
-                <SquareRound></SquareRound>
-
-       
-                
+                <h1>Form</h1>
+                    <div className='container'>
+                        <Text setColor={setColor}></Text>
+                        <Select color={color}></Select>
+                </div>
             </header>
         </div>
     );
