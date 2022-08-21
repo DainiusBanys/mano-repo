11e34151f6799159bbbd586df75267e5126cbbd5
Dainius Banys <b.dainius@gmail.com>
2022-08-21 16:33:10 +0300

    count radom color

--- Is_teorijos/React/07.13/my-app/src/Components/022Mano/count.jsx
+++ Is_teorijos/React/07.13/my-app/src/Components/022Mano/count.jsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
+import randColor from '../../Functions/randomColor'
 
 function Count() {
 
     const [count, setCount] = useState(0);
 
-    const addCount = () => { setCount(count + 1) };
-    const minusCount = () => { setCount(count - 1) };
+    const addCount = () => { (count < 10) ? setCount(count + 1) : setCount(0) };
+    const minusCount = () => { (count > -10) ? setCount(count - 1) : setCount(0) };
 
     return (
 
 
         <>
-            <h2>Skaitiklis: {count}</h2>
+            <h2 style={{color: randColor()}}>Skaitiklis: {count}</h2>
             <button onClick={addCount}>Count plus</button>
             <button onClick={minusCount}>Count minus</button>
         </>
