5a23050995702b02b82324983cd516eb125e02a9
DainiusBanys <b.dainius@gmail.com>
2023-03-13 19:59:21 +0200

    small updates

--- CodeAcademy/3/index.js
+++ CodeAcademy/3/index.js
@@ -1,13 +1,15 @@
-console.log("hello!");
-const sign = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
+const signs = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
 const luck = ['good', 'bad', 'normal', 'bright', 'joyfull', 'colorfull', 'dangerous', 'interesting', 'blue', 'funny', 'fabulous', 'extraordinary'];
 
 const randIndex = () => {
-    return Math.floor(1 + Math.random() * 12);
+    return Math.floor(Math.random() * 12);
 }
 
-console.log(randIndex());
+let sign = prompt("Please enter your sign");
 
-const horoscope = (sign) => {
-    return `Your sign is ${sign}, and your day will be ${luck[randIndex()]}. Your best soulmate is ${sign[randIndex()]}`;
+
+const horoscope = () => {
+    return `Your sign is ${sign}, and your day will be ${luck[randIndex()]}. Your best soulmate is ${signs[randIndex()]}`;
 }
+
+console.log(horoscope());
