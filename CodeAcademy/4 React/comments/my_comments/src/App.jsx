7e14e9900f007f415bdf93bfdae2191e5970dea4
DainiusBanys <b.dainius@gmail.com>
2024-02-07 16:35:57 +0200

    react practice

---
+++ CodeAcademy/4 React/comments/my_comments/src/App.jsx
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import { comments } from "./commentData";
+import Card from "./Card";
+
+function App() {
+  return (
+    <div className="App">
+      {comments.map((comment) => (
+        <Card commentObject={comment} />
+      ))}
+      ;
+    </div>
+  );
+}
+
+export default App;
---
+++ CodeAcademy/4 React/react-food-order-form/src/App.jsx
@@ -0,0 +1,12 @@
+// import "./App.css";
+import FoodOrderForm from "./FoodOrderForm";
+
+function App() {
+  return (
+    <div>
+      <FoodOrderForm />
+    </div>
+  );
+}
+
+export default App;
