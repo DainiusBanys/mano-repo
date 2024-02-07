319ebdcd46a49cbfa54f2539b3120d2d2f0a6838
DainiusBanys <b.dainius@gmail.com>
2024-02-07 17:51:36 +0200

    initial component setup

---
+++ CodeAcademy/4 React/jammming/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Jamming </h1>
+        <input placeholder="Input song name" aria-label="search field" />
+        <button>SEARCH</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ CodeAcademy/4 React/jammming/src/track/Track.jsx
@@ -0,0 +1,5 @@
+function Track() {
+  return null;
+}
+
+export default Track;
