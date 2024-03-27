a1cdc3aa59cfae95421594fd042aa1f0504a01f4
DainiusBanys <b.dainius@gmail.com>
2024-03-27 15:54:07 +0200

    Spotify Playlist final

--- CodeAcademy/4 React/jammming/src/playlist/Playlist.jsx
+++ CodeAcademy/4 React/jammming/src/playlist/Playlist.jsx
@@ -1,16 +1,29 @@
 import Tracklist from "../tracklist/Tracklist";
+import styles from "./Playlist.module.css";
+import React from "react";
 
 function Playlist(props) {
+  function handleNameChange({ target }) {
+    props.onNameChange(target.value);
+  }
+
   return (
-    <div className="PlayList">
+    <div className={styles.Playlist}>
       <input
         type="text"
         defaultValue={"New Playlist"}
         aria-label="play list input field"
+        onChange={handleNameChange}
       />
-      <Tracklist trackObject={props.playlistTracks} />
+      <Tracklist
+        trackObject={props.playlistTracks}
+        onRemove={props.onRemove}
+        isRemoval={true}
+      />
       {/* add track list component */}
-      <button className="Playlist-save">SAVE TO SPOTIFY</button>
+      <button className={styles["Playlist-save"]} onClick={props.onSave}>
+        SAVE TO SPOTIFY
+      </button>
     </div>
   );
 }
